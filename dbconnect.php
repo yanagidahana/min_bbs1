9530a36de04fee8106f74fc2444fb0ec658043a5
yanagidahana <hana878787zzz@gmail.com>
2021-01-10 00:38:36 +0900

    id 変更

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-  $db = new PDO('mysql:host=localhost;dbname=min_bbs1;charset=utf8','root','root');
+  $db = new PDO('mysql:host=localhost;dbname=min_bbs1;charset=utf8','root','');
 
 } catch(PDOException $e) {
   print('DB接続エラー:' . $e->getMessage());
